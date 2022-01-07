b5520365786e60dfcf09f8190d3b89c13a7417b7
Ruben van Gulik <rubenvangulik@gmail.com>
2022-01-07 12:26:03 +0100

    Put VRAM drawing in Debug class

--- debug.cpp
+++ debug.cpp
@@ -1,8 +1,7 @@
 #include "debug.h"
 
+#include <SDL2/SDL.h>
 #include <iostream>
-#include <bitset>
-#include <iomanip>
 #define FMT_HEADER_ONLY
 #include "fmt/format.h"
 
@@ -29,4 +28,43 @@ void Debug::draw(int x, int y) {
 
     // Timer
     FC_Draw(m_font, m_renderer, x + 20, 140, fmt::format("DIV={0:2X} TIMA={1:2X} TMA={2:2X} TAC={3:2X}", m_gb->mmu.divide_register, m_gb->mmu.timer_counter, m_gb->mmu.timer_modulo, m_gb->mmu.timer_control).c_str());
+
+
+    // VRAM
+    unsigned int palette[4] = {0xFFFFFFFF,0xAAAAAAFF,0x555555FF,0x000000FF};
+    std::vector<int> conv;
+    conv.resize(16*24*8*8);
+
+    // Convert the gameboy tiledata of a series of 64 values to an array
+    // that we can draw on the screen
+    for (int tile_x = 0; tile_x < 16; tile_x++) {
+        for (int tile_y = 0; tile_y < 24; tile_y++) {
+            for (int pixel = 0; pixel < 8*8; pixel++) {
+                int tile_number = tile_x * 16 + tile_y;
+                int screen_pixel = tile_y * 16 * 64 + (int)(pixel/8) * 16 * 8 + tile_x * 8 + pixel % 8;
+
+                conv[screen_pixel] = palette[m_gb->gpu.tileset[tile_number*64 + pixel]];
+            }
+        }
+    }
+
+    // 16x24 tiles of 8x8 pixels
+    SDL_Surface* screen = SDL_CreateRGBSurfaceFrom(
+        &conv[0],
+        16*8, // width
+        24*8, // height
+        32, // bits per pixel
+        16*8*4, // bytes per row of pixels
+        0xFF000000,
+        0x00FF0000,
+        0x0000FF00,
+        0x000000FF);
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(m_renderer, screen);
+    SDL_FreeSurface(screen);
+
+    // Render the gameboy texture onto the screen
+    SDL_Rect dst = {x + 400, 20, 16*8*2, 24*8*2};
+    SDL_RenderCopy(m_renderer, texture, NULL, &dst);
+    SDL_DestroyTexture(texture);
 }
--- main.cpp
+++ main.cpp
@@ -29,9 +29,9 @@ int main(int argc, char* args[])
     //GameBoy gb("C:\\Users\\ruben\\Documents\\GitHub\\gameboy\\roms\\cpu_instrs.gb");
     //GameBoy gb("C:\\Users\\ruben\\Documents\\GitHub\\gameboy\\roms\\02-interrupts.gb");
     GameBoy gb("C:\\Users\\ruben\\Documents\\GitHub\\gameboy\\roms\\Tetris (World) (Rev A).gb");
+    //GameBoy gb("C:\\Users\\ruben\\Documents\\GitHub\\gameboy\\roms\\Dr. Mario (World).gb");
     //GameBoy gb("roms/Dr. Mario (World).gb");
     //GameBoy gb("C:/Users/Ruben/Documents/ROMs/GameBoy/cpu_instrs/cpu_instrs.gb");
-    gb.debug_mode = false;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "Init error: " << SDL_GetError() << std::endl;
@@ -67,6 +67,8 @@ int main(int argc, char* args[])
     bool redraw = false;
 
     bool stepping_mode = false;
+    u8 break_instr = 0;
+    u16 break_PC = 0;
 
     const Uint8* keys = SDL_GetKeyboardState(NULL);
 
@@ -117,8 +119,10 @@ int main(int argc, char* args[])
         while (!redraw && !stepping_mode) {
             gb.cycle();
 
-            //if (gb.cpu.current_opcode() == 0x76)
-            //    stepping_mode = true;
+            if (break_instr != 0 && gb.cpu.current_opcode() == break_instr)
+                stepping_mode = true;
+            if (break_PC != 0 && gb.cpu.PC == break_PC)
+                stepping_mode = true;
             
             redraw = gb.gpu.get_redraw();
         }
@@ -151,45 +155,6 @@ int main(int argc, char* args[])
 
         // Debug info rendering
         debug.draw(SCREEN_W, 0);
-
-
-        // Tileset data
-        unsigned int palette[4] = {0xFFFFFFFF,0xAAAAAAFF,0x555555FF,0x000000FF};
-        std::vector<int> conv;
-        conv.resize(16*24*8*8);
-
-        // Convert the gameboy tiledata of a series of 64 values to an array
-        // that we can draw on the screen
-        for (int tile_x = 0; tile_x < 16; tile_x++) {
-            for (int tile_y = 0; tile_y < 24; tile_y++) {
-                for (int pixel = 0; pixel < 8*8; pixel++) {
-                    int tile_number = tile_x * 16 + tile_y;
-                    int screen_pixel = tile_y * 16 * 64 + (int)(pixel/8) * 16 * 8 + tile_x * 8 + pixel % 8;
-
-                    conv[screen_pixel] = palette[gb.gpu.tileset[tile_number*64 + pixel]];
-                }
-            }
-        }
-
-        // 16x24 tiles of 8x8 pixels
-        screen = SDL_CreateRGBSurfaceFrom(
-            &conv[0],
-            16*8, // width
-            24*8, // height
-            32, // bits per pixel
-            16*8*4, // bytes per row of pixels
-            0xFF000000,
-            0x00FF0000,
-            0x0000FF00,
-            0x000000FF);
-
-        texture = SDL_CreateTextureFromSurface(renderer, screen);
-        SDL_FreeSurface(screen);
-
-        // Render the gameboy texture onto the screen
-        dst = {SCREEN_W + 400, 20, 16*8*2, 24*8*2};
-        SDL_RenderCopy(renderer, texture, NULL, &dst);
-        SDL_DestroyTexture(texture);
 
         // Update the screen
         SDL_RenderPresent(renderer);
